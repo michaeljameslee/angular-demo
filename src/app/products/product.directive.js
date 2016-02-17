d24d03ec43d97f043b0bd52f6528b924e790fe05
Mike Lee <mike@mvpindex.com>
2016-02-17 02:39:55 -0600

    Step 3 complete - Product directive and styling

---
+++ src/app/products/product.directive.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('angularInterview')
+  .directive('product', function(){
+  return {
+    restrict: 'E',
+    scope: {
+      product: '='
+    },
+    templateUrl: 'app/products/product.view.html',
+    link: function(scope, elem, attrs) {
+
+    }
+  }
+});
